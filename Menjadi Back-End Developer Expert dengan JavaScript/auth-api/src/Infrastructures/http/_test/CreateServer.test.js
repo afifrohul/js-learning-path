3230f47ddb4ee7691423cf26eba5c8a72cc85bad
afifrohul <afifmemyself22@gmail.com>
2026-04-16 11:09:08 +0700

    fix(auth-api): createServer.test.js in general http server

--- Menjadi Back-End Developer Expert dengan JavaScript/auth-api/src/Infrastructures/http/_test/CreateServer.test.js
+++ Menjadi Back-End Developer Expert dengan JavaScript/auth-api/src/Infrastructures/http/_test/CreateServer.test.js
@@ -6,7 +6,12 @@ import container from "../../container.js";
 import createServer from "../CreateServer.js";
 
 describe("HTTP server", () => {
+  beforeAll(() => {
+    vi.spyOn(console, "error").mockImplementation(() => {});
+  });
+
   afterAll(async () => {
+    console.error.mockRestore();
     await pool.end();
   });
 
@@ -15,16 +20,34 @@ describe("HTTP server", () => {
     await AuthenticationsTableTestHelper.cleanTable();
   });
 
-  describe("when POST /users", () => {
-    it("should response 404 when request unregistered route", async () => {
-      // Arrange
-      const app = await createServer({});
-      // Action
-      const response = await request(app).get("/unregisteredRoute");
-      // Assert
-      expect(response.statusCode).toEqual(404);
+  it("should response 404 when request unregistered route", async () => {
+    // Arrange
+    const app = await createServer({});
+    // Action
+    const response = await request(app).get("/unregisteredRoute");
+    // Assert
+    expect(response.statusCode).toEqual(404);
+  });
+
+  it("should handle server error correctly", async () => {
+    const fakeContainer = {
+      getInstance: () => {
+        throw new Error("unexpected error");
+      },
+    };
+
+    const app = await createServer(fakeContainer);
+
+    const response = await request(app).post("/users").send({
+      username: "dicoding",
+      password: "secret",
+      fullname: "Dicoding Indonesia",
     });
 
+    expect(response.status).toEqual(500);
+  });
+
+  describe("when POST /users", () => {
     it("should response 201 and persisted user", async () => {
       // Arrange
       const requestPayload = {
@@ -129,24 +152,6 @@ describe("HTTP server", () => {
       expect(response.status).toEqual(400);
       expect(response.body.status).toEqual("fail");
       expect(response.body.message).toEqual("username tidak tersedia");
-    });
-
-    it("should handle server error correctly", async () => {
-      // Arrange
-      const requestPayload = {
-        username: "dicoding",
-        fullname: "Dicoding Indonesia",
-        password: "super_secret",
-      };
-      const app = await createServer({}); // fake container
-      // Action
-      const response = await request(app).post("/users").send(requestPayload);
-      // Assert
-      expect(response.status).toEqual(500);
-      expect(response.body.status).toEqual("error");
-      expect(response.body.message).toEqual(
-        "terjadi kegagalan pada server kami",
-      );
     });
   });
 
