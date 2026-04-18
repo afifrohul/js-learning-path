53a022098d6ccc411791e7c7769e0712058ac5ad
afifrohul <afifmemyself22@gmail.com>
2026-04-18 20:32:06 +0700

    feat(forum-api): setup table threads

---
+++ Menjadi Back-End Developer Expert dengan JavaScript/forum-api/migrations/1776515830804_create-table-threads.js
@@ -0,0 +1,49 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable("threads", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    user_id: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    title: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+    created_at: {
+      type: "TIMESTAMP",
+      notNull: true,
+    },
+    updated_at: {
+      type: "TIMESTAMP",
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint(
+    "threads",
+    "fk_threads.owner_users.id",
+    "FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE",
+  );
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable("threads");
+};
