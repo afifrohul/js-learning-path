d865dd4ccb08a69aa13d6f8b7c0c2ffc2409e849
afifrohul <afifmemyself22@gmail.com>
2026-05-20 10:03:52 +0700

    feat(contacts-app): add react router

--- Belajar Fundamental Aplikasi Web dengan React/contacts-app/src/components/DeleteButton.jsx
+++ Belajar Fundamental Aplikasi Web dengan React/contacts-app/src/components/DeleteButton.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React from "react";
+import { FiDelete } from "react-icons/fi";
 
 function DeleteButton(props) {
   const { id, onDelete } = props;
-  
-  return <button className='contact-item__delete' onClick={() => onDelete(id)}>X</button>
+
+  return (
+    <button className="contact-item__delete" onClick={() => onDelete(id)}>
+      <FiDelete />
+    </button>
+  );
 }
 
 export default DeleteButton;
