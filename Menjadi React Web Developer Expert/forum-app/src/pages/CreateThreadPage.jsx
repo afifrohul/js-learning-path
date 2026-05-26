dbf6e5d1dc266d4a915131c83e23f34e53e9ab80
afifrohul <afifmemyself22@gmail.com>
2026-05-26 15:43:35 +0700

    feat(forum-app): add conditional rendering in create thread page

--- Menjadi React Web Developer Expert/forum-app/src/pages/CreateThreadPage.jsx
+++ Menjadi React Web Developer Expert/forum-app/src/pages/CreateThreadPage.jsx
@@ -1,10 +1,10 @@
 import ThreadInput from '@/components/thread-input';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { asyncAddThread } from '@/states/threads/action';
 import { useDispatch, useSelector } from 'react-redux';
 
 export default function CreateThreadPage() {
-  const loading = useSelector((states) => states.loading);
+  const { authUser, loading } = useSelector((states) => states);
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -16,11 +16,22 @@ export default function CreateThreadPage() {
   };
 
   return (
-    <div className=" mt-2">
-      <p className="text-sm font-semibold">Create New Thread</p>
-      <div className="w-full">
-        <ThreadInput createThread={onAddThread} loading={loading} />
-      </div>
-    </div>
+    <>
+      {authUser ? (
+        <div className=" mt-2">
+          <p className="text-sm font-semibold">Create New Thread</p>
+          <div className="w-full">
+            <ThreadInput createThread={onAddThread} loading={loading} />
+          </div>
+        </div>
+      ) : (
+        <div className="text-sm">
+          <Link to="/sign-in" className="underline">
+            Sign in
+          </Link>{' '}
+          to add thread
+        </div>
+      )}
+    </>
   );
 }
