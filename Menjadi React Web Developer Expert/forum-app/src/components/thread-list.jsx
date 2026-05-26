95ee20cf1541da4ff6cbeeef0e0e60e8332149f5
afifrohul <afifmemyself22@gmail.com>
2026-05-26 11:26:17 +0700

    feat(forum-app): conditional rendiring if threads empty

--- Menjadi React Web Developer Expert/forum-app/src/components/thread-list.jsx
+++ Menjadi React Web Developer Expert/forum-app/src/components/thread-list.jsx
@@ -1,6 +1,14 @@
 import ThreadItem from '@/components/thread-item';
 
 export default function ThreadList({ threads }) {
+  if (threads.length <= 0) {
+    return (
+      <div className="space-y-4 mt-3">
+        <p className="text-xs text-center">Thread not found</p>
+      </div>
+    );
+  }
+
   return (
     <div className="space-y-4 mt-3">
       {threads.map((thread, index) => (
