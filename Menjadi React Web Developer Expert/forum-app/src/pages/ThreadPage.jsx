27e0a3db4dec9fc1acdebbc2d7367dc19eefe154
afifrohul <afifmemyself22@gmail.com>
2026-05-26 11:59:49 +0700

    feat(forum-app): create filter thread

--- Menjadi React Web Developer Expert/forum-app/src/pages/ThreadPage.jsx
+++ Menjadi React Web Developer Expert/forum-app/src/pages/ThreadPage.jsx
@@ -1,9 +1,18 @@
 import ThreadList from '@/components/thread-list';
 import { asyncPopulateUsersAndThreads } from '@/states/shared/action';
 import { Plus } from 'lucide-react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
 
 function ThreadPage() {
   const {
@@ -11,6 +20,8 @@ function ThreadPage() {
     users = [],
     authUser,
   } = useSelector((states) => states);
+
+  const [selectedCategory, setSelectedCategory] = useState('all');
 
   const dispatch = useDispatch();
 
@@ -21,28 +32,60 @@ function ThreadPage() {
   const threadList = threads.map((thread) => ({
     ...thread,
     user: users.find((user) => user.id === thread.ownerId),
-    authUser: authUser.id,
+    authUser: authUser?.id,
   }));
+
+  const categoryList = ['all', ...new Set(threads.map((t) => t.category))];
+
+  const filteredThreadList =
+    selectedCategory === 'all'
+      ? threadList
+      : threadList.filter((thread) => thread.category === selectedCategory);
 
   return (
     <div>
-      <div>
+      <div className="mt-2">
         <div className="flex items-center justify-between">
           <p className="text-sm font-semibold">Thread available</p>
-          <Link
-            to="/threads/add"
-            className="border hover:bg-accent py-0.5 px-1 rounded duration-200 transition-all"
-          >
-            <div className="flex gap-1 items-center">
-              <Plus className="w-3.5 h-3.5" />
-              <p className="text-xs font-semibold">Create New Thread</p>
+          <div className="flex items-center gap-2">
+            <div className="flex items-center gap-2">
+              <p className="text-xs">Filter thread</p>
+              <Select
+                value={selectedCategory}
+                onValueChange={setSelectedCategory}
+              >
+                <SelectTrigger className="w-48">
+                  <SelectValue placeholder="Filter thread" />
+                </SelectTrigger>
+
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Category / Tag</SelectLabel>
+
+                    {categoryList.map((item) => (
+                      <SelectItem key={item} value={item}>
+                        {item}
+                      </SelectItem>
+                    ))}
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
             </div>
-          </Link>
+            <Link
+              to="/threads/add"
+              className="border hover:bg-accent py-0.5 px-1 rounded duration-200 transition-all"
+            >
+              <div className="flex gap-1 items-center py-0.5">
+                <Plus className="w-3.5 h-3.5" />
+                <p className="text-xs font-semibold">Create New Thread</p>
+              </div>
+            </Link>
+          </div>
         </div>
-        <ThreadList threads={threadList} />
+
+        <ThreadList threads={filteredThreadList} />
       </div>
     </div>
   );
 }
-
 export default ThreadPage;
